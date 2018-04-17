3aa46cf15ee66630780d3b80a7e63c69dca9726f
ZzSaitozZ <hiragasaito951@gmail.com>
2018-04-17 11:41:50 +0700

    abv

--- App_Start/BundleConfig.cs
+++ App_Start/BundleConfig.cs
@@ -9,23 +9,26 @@ namespace RealEstate
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+                        "~/Resources/Scripts/jquery-{version}.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-                        "~/Scripts/jquery.validate*"));
+                        "~/Resources/Scripts/jquery.validate*"));
 
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at https://modernizr.com to pick only the tests you need.
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-                        "~/Scripts/modernizr-*"));
+                        "~/Resources/Scripts/modernizr-*"));
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js",
-                      "~/Scripts/respond.js"));
+                      "~/Resources/Scripts/bootstrap.js",
+                      "~/Resources/Scripts/respond.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Resources/CSS/bootstrap.css",
+                      "~/Resources/CSS/site.css"));
+
+            bundles.Add(new StyleBundle("~/Content/Home").Include(
+                      "~/Resources/CSS/Home/Banner.css"));
         }
     }
 }
