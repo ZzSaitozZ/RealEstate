f7fd5a0b681356102c74c78b5544e0fd444e30b3
ZzSaitozZ <hiragasaito951@gmail.com>
2018-04-19 22:46:06 +0700

    Thêm thử Areas mới

---
+++ Areas/Details/DetailsAreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace RealEstate.Areas.Details
+{
+    public class DetailsAreaRegistration : AreaRegistration 
+    {
+        public override string AreaName 
+        {
+            get 
+            {
+                return "Details";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            context.MapRoute(
+                "Details_default",
+                "Details/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
